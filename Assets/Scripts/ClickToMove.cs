f41aa5520275f717a2c7ec754546282eab9b6192
Viraj Malia <virajmalia2@gmail.com>
2017-10-02 12:42:36 -0400

    Sam's work on Sunday

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace CompletedProject
+{
+
+	public class ClickToMove : MonoBehaviour
+	{
+		private NavMeshAgent navMeshAgent;
+		private Transform target;
+
+		// Use this for initialization
+		void Awake()
+		{
+			navMeshAgent = GetComponent<NavMeshAgent>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Input.GetButtonDown("Fire2"))
+			{
+				if (Physics.Raycast(ray, out hit, 1000)){
+					navMeshAgent.destination = hit.point;
+				}
+			}
+		}
+	}
+
+}
