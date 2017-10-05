69e8c09c090f619451008d0fedd79df5ec450b9f
Viraj Malia <virajmalia2@gmail.com>
2017-10-05 13:35:02 -0400

    Director script with latest update

---
+++ Assets/Scripts/agentMsg.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agentMsg : MonoBehaviour {
+
+	private List<GameObject> selectedUnit = new List<GameObject>();
+	private List<Renderer> rend = new List<Renderer>();
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if(Physics.Raycast(ray, out hit)){
+			if (hit.transform.tag == "Player" && Input.GetMouseButtonDown(0)){
+				selectedUnit.Add(hit.transform.gameObject);		// Add selected players to list
+				// Change color of each selected player
+				foreach( GameObject obj in selectedUnit ){
+					rend.Add(obj.GetComponent<Renderer> ());
+					foreach (Renderer r in rend) {
+						r.material.color = Color.cyan;
+					}
+					obj.SendMessage("Select", 1);
+				}
+			}
+
+			if (hit.transform.tag == "Ground" && Input.GetMouseButtonDown(2)){
+				// Destination for every selected object
+				foreach( GameObject obj in selectedUnit){
+					obj.SendMessage("Destination", hit.point);
+				}
+			}
+
+			if (hit.transform.tag == "Ground" && Input.GetMouseButtonDown(0)){
+				// Deselect and change back color
+				foreach( GameObject obj in selectedUnit ){
+					foreach (Renderer r in rend) {
+						r.material.color = Color.white;
+					}
+					obj.SendMessage("Deselect", 1);
+				}
+				selectedUnit.Clear();
+				rend.Clear();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/agentSelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agentSelect : MonoBehaviour {
+
+	//public Transform enemy;
+	private NavMeshAgent goodGuy;
+	private bool set = false;
+	private bool selected = false;
+	private Vector3 MoveTo;
+
+	// Use this for initialization
+	void Start () {
+		goodGuy = gameObject.GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected && set) {
+			set = true;
+			goodGuy.SetDestination (MoveTo);
+		}
+	}
+
+	void Select(int x){
+		selected = true;
+	}
+
+	void Deselect(int x){
+		selected = false;
+	}
+
+	void Destination(Vector3 d){
+		MoveTo = d;
+		set = true;
+	}
+}
