cb9b5f1d0f3875bb136c5412d8e7c8d02b502022
nikitabala19 <nikitabala19@gmail.com>
2017-10-06 15:47:03 -0400

    Door obstacle

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private int flag = 0;
+    RaycastHit hitInfo = new RaycastHit();
+    // Use this for initialization
+    void Start()
+    {
+        flag = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo) && hitInfo.transform.tag == "Door")
+            {
+                if (flag == 0)
+                {
+                    print("It's working");
+                    hitInfo.transform.Rotate(0, 60, 0, Space.Self);
+                    flag = 1;
+                }
+                else
+                {
+                    print("Else");
+                    hitInfo.transform.Rotate(0, -60, 0, Space.Self);
+                    flag = 0;
+                }
+            }
+        }
+    }
+}
